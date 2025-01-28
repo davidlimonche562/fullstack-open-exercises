11780bd6cfac294af06bb5cfe07af300e697a95a
davidlimonche562 <davidlimonche@562gmail.com>
2025-01-28 18:11:31 -0400

    Refactor app to pass the entire course object to Content and Total components for exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h1>{course.name}</h1>;
 
 const Part = ({ part, exercises }) => {
   return (
@@ -8,41 +8,43 @@ const Part = ({ part, exercises }) => {
   );
 };
 
-const Content = ({ parts}) => (
+const Content = ({course}) => (
   <div>
-    <Part part={parts[0].name} exercises={parts[0].exercises} />
-    <Part part={parts[1].name} exercises={parts[1].exercises} />
-    <Part part={parts[2].name} exercises={parts[2].exercises} />
+    <Part part={course.parts[0].name} exercises={course.parts[0].exercises} />
+    <Part part={course.parts[1].name} exercises={course.parts[1].exercises} />
+    <Part part={course.parts[2].name} exercises={course.parts[2].exercises} />
   </div>
 );
 
-const Total = ({ parts}) => {
-  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+const Total = ({ course}) => {
+  const totalExercises = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
   return <p>Number of exercises {totalExercises}</p>;
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Content course={course}/>
+      <Total course={course} />
     </div>
   );
 };
