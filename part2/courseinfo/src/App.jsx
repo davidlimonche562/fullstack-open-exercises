94497fb5146179014dff83a31a35bb6da662e799
davidlimonche562 <davidlimonche@562gmail.com>
2025-01-30 18:10:51 -0400

    feat: Add total exercises sum using reduce for course info (exercises 2.2 & 2.3)

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -3,10 +3,19 @@ const Course = ({ course }) => {
     <div>
       <Header name={course.name} /> 
       <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   );
 };
 
+
+
+const Total = ({parts}) =>{
+  const total = parts.reduce((sum,part) => sum + part.exercises, 0);
+  return(
+    <p><strong>total of {total} exercises </strong></p>
+  )
+}
 const Header = ({ name }) => <h1>{name}</h1>;
 
 const Content = ({ parts }) => {
