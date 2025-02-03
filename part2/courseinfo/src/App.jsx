14b58191dd6369b446b0779c8b44d6ff81aa4eb9
davidlimonche562 <davidlimonche@562gmail.com>
2025-02-03 16:11:57 -0400

    refactor: modularize Course component and its subcomponents - step 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,38 +1,4 @@
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} /> 
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  );
-};
-
-
-
-const Total = ({parts}) =>{
-  const total = parts.reduce((sum,part) => sum + part.exercises, 0);
-  return(
-    <p><strong>total of {total} exercises </strong></p>
-  )
-}
-const Header = ({ name }) => <h1>{name}</h1>;
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  );
-};
-
-
-const Part = ({ part }) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  );
-};
-
+import Course from "./Course";
 
 const App = () => {
   const courses = [
