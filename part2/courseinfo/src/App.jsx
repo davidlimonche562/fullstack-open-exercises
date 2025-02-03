f405acf855491dbf3a3167f05f812d77d1219c66
davidlimonche562 <davidlimonche@562gmail.com>
2025-02-03 16:08:42 -0400

    feat: implement dynamic course rendering with components and total exercises calculation - step 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -35,17 +35,33 @@ const Part = ({ part }) => {
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      { name: 'Fundamentals of React', exercises: 10, id: 1 },
-      { name: 'Using props to pass data', exercises: 7, id: 2 },
-      { name: 'State of a component', exercises: 14, id: 3 },
-    ],
-  };
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        { name: 'Fundamentals of React', exercises: 10, id: 1 },
+        { name: 'Using props to pass data', exercises: 7, id: 2 },
+        { name: 'State of a component', exercises: 14, id: 3 },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        { name: 'Routing', exercises: 3, id: 1 },
+        { name: 'Middlewares', exercises: 7, id: 2 },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
